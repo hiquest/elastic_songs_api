e87c63bc7af3ad20f8afb18df81486410016b690
Ianis Triandafilov <janis.sci@gmail.com>
2022-01-12 21:57:06 +0100

    Import csv

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,18 @@
+require 'csv'
+
 class Song < ApplicationRecord
   include Searchable
+
+  def self.import_csv!
+    filepath = "/Users/yanis/Downloads/tcc_ceds_music.csv"
+    res = CSV.parse(File.read(filepath), headers: true)
+    res.each_with_index do |s, ind|
+      Song.create!(
+        artist: s["artist_name"],
+        title: s["track_name"],
+        genre: s["genre"],
+        lyrics: s["lyrics"]
+      )
+    end
+  end
 end
